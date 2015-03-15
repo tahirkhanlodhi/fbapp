5d6bc1192f851f5942fa4211b945f29f8b7935ea
tahirkhanlodhi <tahir_lodhi09@yahoo.com>
2015-03-15 18:54:51 +0500

    fb sign out

--- config/routes.rb
+++ config/routes.rb
@@ -25,16 +25,16 @@ Rails.application.routes.draw do
       :registrations => "users/registrations",
 
   }
-  devise_scope :users do
-    #get "admin", :to => "devise/sessions#new"
-    #get "sign_in", :to => "users/sessions#new"
-    get 'sign_in', :to => 'users/sessions#new', :as => :new_session
-    #get "sign_out", :to => "users/sessions#destroy"
-    post "sign_out", :to => "users/sessions#destroy"
-    get "sign_up", :to => "users/registrations#new"
-    patch "/users/update_user", :to => "users/registrations#update_user"
-    #get "login", :to => "devise/sessions#login"
-  end
+  #devise_scope :users do
+  #  #get "admin", :to => "devise/sessions#new"
+  #  #get "sign_in", :to => "users/sessions#new"
+  #  get 'sign_in', :to => 'users/sessions#new', :as => :new_session
+  #  #get "sign_out", :to => "users/sessions#destroy"
+  #  post "sign_out", :to => "users/sessions#destroy"
+  #  get "sign_up", :to => "users/registrations#new"
+  #  patch "/users/update_user", :to => "users/registrations#update_user"
+  #  #get "login", :to => "devise/sessions#login"
+  #end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
