4b359c29bf3bccca3a4a3fb5a3fdebe2df70beb4
tahirkhanlodhi <tahir_lodhi09@yahoo.com>
2015-03-15 18:44:38 +0500

    git session change

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,143 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  layout 'application'
+
+  def new
+    resource = build_resource({})
+    respond_with resource
+  end
+
+  def create
+
+  end
+
+  def destroy
+    resource.destroy
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    set_flash_message :notice, :destroyed if is_navigational_format?
+    respond_with_navigational(resource) { redirect_to after_sign_out_path_for(resource_name) }
+  end
+
+
+  #def create
+  #  $read = user_params
+  #  $role = params["role"]
+  #  $country = params[:country]
+  #  $address = {:post_code => params[:postal_code], :city => params[:locality],
+  #              :state => params[:administrative_area_level_1], :country => params[:country], :street_address => params[:name], :latitude => params[:lat], :longitude => params[:lng]}
+  #
+  #  if params[:country] == 'United Kingdom' || params[:country] == 'GB' || params[:country] == 'UK'
+  #
+  #    $show = true;
+  #    redirect_to "/admin/register_merchant_form2"
+  #  else
+  #    @user = SubscribedUser.new
+  #    @user.name = params[:user][:name]
+  #    @user.email = params[:user][:email]
+  #    @user.address_line = params[:user][:profile_attributes][:address_line1]
+  #
+  #    if @user.save
+  #      @user.update_attributes(:post_code => params[:postal_code], :city => params[:locality],
+  #                              :state => params[:administrative_area_level_1], :country => params[:country], :street_address => params[:name])
+  #      #subscribeduser.update_attribute(:address, params[:address_line1])
+  #      $show = false;
+  #      redirect_to "/homes/country_error"
+  #    end
+  #  end
+  #end
+
+  #def update_user
+  #  $read = user_params
+  #  $role = params["role"]
+  #  $country = params[:country]
+  #  $address = {:post_code => params[:postal_code], :city => params[:locality],
+  #              :state => params[:administrative_area_level_1], :country => params[:country], :street_address => params[:name], :latitude => params[:lat], :longitude => params[:lng]}
+  #
+  #  if params[:country] == 'United Kingdom' || params[:country] == 'GB' || params[:country] == 'UK'
+  #
+  #    redirect_to "/admin/register_merchant_form2"
+  #  else
+  #    @user = SubscribedUser.new
+  #    @user.name = params[:user][:name]
+  #    @user.email = params[:user][:email]
+  #    @user.address_line = params[:user][:profile_attributes][:address_line1]
+  #
+  #    if @user.save
+  #      @user.update_attributes(:post_code => params[:postal_code], :city => params[:locality],
+  #                              :state => params[:administrative_area_level_1], :country => params[:country], :street_address => params[:name])
+  #      #subscribeduser.update_attribute(:address, params[:address_line1])
+  #      redirect_to "/homes/country_error"
+  #    end
+  #  end
+  #end
+
+  #private
+
+  #def user_params
+  #  params.require(:user).permit(:name, :email, :password, :password_confirmation, :unique_name, :admin_username, :confirmed_at, :profile_attributes => [:first_name, :last_name, :age, :gender, :post_code, :url, :address_line1, :id, :country, :city, :street_address, :state])
+  #end
+  #
+  #def subscribed_user_params
+  #  params.require(:subscribed_user).permit(:name, :email, :address)
+  #end
+
+  # before_filter :configure_sign_up_params, only: [:create]
+  # before_filter :configure_account_update_params, only: [:update]
+
+  # GET /resource/sign_up
+  # def new
+  #   super
+  # end
+
+  # POST /resource
+  # def create
+  #   super
+  # end
+
+  # GET /resource/edit
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource
+  # def update
+  #   super
+  # end
+
+  # DELETE /resource
+  # def destroy
+  #   super
+  # end
+
+  # GET /resource/cancel
+  # Forces the session data which is usually expired after sign
+  # in to be expired now. This is useful if the user wants to
+  # cancel oauth signing in/up in the middle of the process,
+  # removing all OAuth session data.
+  # def cancel
+  #   super
+  # end
+
+  # protected
+
+  # You can put the params you want to permit in the empty array.
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.for(:sign_up) << :attribute
+  # end
+
+  # You can put the params you want to permit in the empty array.
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.for(:account_update) << :attribute
+  # end
+
+  # The path used after sign up.
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+
+  # The path used after sign up for inactive accounts.
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
 # before_filter :configure_sign_up_params, only: [:create]
 # before_filter :configure_account_update_params, only: [:update]
 
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,98 @@
 class Users::SessionsController < Devise::SessionsController
+  require 'securerandom'
+  layout "application"
+  skip_before_filter  :verify_authenticity_token
+
+
+  def create
+    begin
+      puts "________________________________________________________"
+      puts "________________________Sign In_________________________"
+      puts "________________________________________________________"
+      puts "000000000000000000000",params.inspect
+      puts "............params[:device_token]............",params[:user][:device_token].inspect
+      puts "............params[:user]............",params[:user]
+      puts "............request.format(request............",request.format(request)
+      puts "............request.format............",request.format
+      if request.format(request) == '*/*'
+        #params[:device_token] = "askldjfh3a2s1fgasdf2s1dfsd2fasdf1"
+        resource = warden.authenticate(:scope => resource_name, :recall => "#{controller_path}#failure")
+        if (resource.present?)
+          sign_out(current_user) if current_user.present?
+          puts "............resource.inspect............",resource.inspect
+          random_str = SecureRandom.hex
+          puts "............random_str............",random_str
+          resource.update_attributes(:device_token => params[:user][:device_token], :session_token => random_str)
+          puts "............resource.inspect............",resource.inspect
+          workoutfitcoins = Workoutfitcoin.all.where(:user_id => resource.id)
+          total_coins = workoutfitcoins.sum(:bitcoins) if workoutfitcoins.length > 0
+          return render :json => {:success => "true", :user => {:id => resource.id,:total_coins => total_coins.present? ? total_coins : 0, :first_name => resource.first_name, :last_name => resource.last_name,:age => resource.age, :sex => resource.sex,:weight => resource.weight,:email => resource.email, :token => resource.session_token}, :message => "Signed in Successfully!"}
+          #:id => resource.id,  return render :json => {:success => "true", :message => "Logged in Successfully !", :data => after_sign_in_data(resource, random_str)}
+        else
+          render :json => {:success => "false", :errors => "Login failed"}
+        end
+      else
+        p "(((((((((((((else[params])))))))))))))))", params.inspect
+        @user = User.where(:email => params[:user][:email]).first
+        if (@user && @user.is_admin == true)
+          super
+        else
+          redirect_to "/"
+        end
+      end
+    rescue Exception => e
+      return render :json => {:success => "false", :message => "#{e.message}"}
+    end
+  end
+
+  def destroy
+    puts "********************+++++++++++++Sign Out++++++++++++********************",params.inspect
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
+    yield if block_given?
+    respond_to_on_destroy
+  end
+
+  def respond_to_on_destroy
+    if request.format(request) == '*/*'
+      render :json => {:success => "true", :message => "Sign out successfully"}
+    else
+      respond_to do |format|
+        format.all { head :no_content }
+        format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
+
+      end
+    end
+  end
+
+  def failure
+    return render :json => {:success => "false", :errors => "Login failed."}
+  end
+
+# before_filter :configure_sign_in_params, only: [:create]
+
+# GET /resource/sign_in
+# def new
+#   super
+# end
+
+# POST /resource/sign_in
+# def create
+#   super
+# end
+
+# DELETE /resource/sign_out
+# def destroy
+#   super
+# end
+
+# protected
+
+# You can put the params you want to permit in the empty array.
+# def configure_sign_in_params
+#   devise_parameter_sanitizer.for(:sign_in) << :attribute
+# end
+
 # before_filter :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
